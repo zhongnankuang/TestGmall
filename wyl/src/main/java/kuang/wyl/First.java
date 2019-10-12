3075a16df4bbf27dcb6010b5b031c92abe4b0aa0
wang <wang@yl.com>
2019-10-13 01:04:11 +0800

    Go!!!

---
+++ wyl/src/main/java/kuang/wyl/First.java
@@ -0,0 +1,4 @@
+package kuang.wyl;
+
+public class First {
+}
---
+++ wyl/src/main/java/kuang/wyl/WylApplication.java
@@ -0,0 +1,13 @@
+package kuang.wyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WylApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WylApplication.class, args);
+	}
+
+}
